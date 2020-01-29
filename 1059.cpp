d7faaf88c1fb6bc937099e3f8e5f6dc3d25aced4
Marco <marco.schneider452@gmail.com>
2020-01-29 17:28:36 -0200

    Accepted

---
+++ 1059.cpp
@@ -0,0 +1,21 @@
+//URI Online Judge - Iniciantes - 1059
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+
+    for(i=1; i<=100; i++)
+    {
+        if(i%2 == 0)
+        {
+            cout << i << endl;
+        }
+    }
+    cout << "\n";
+
+    return 0;
+}
