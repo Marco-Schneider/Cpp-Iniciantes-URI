f99aa0b93e795a562b4441309ec1d8dbc78636c0
Marco <marco.schneider452@gmail.com>
2020-01-29 22:03:48 -0200

    Accepted

---
+++ 1078.cpp
@@ -0,0 +1,19 @@
+//URI Online Judge - Iniciantes - 1078
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    for(int i=1; i<=10; i++)
+    {
+        cout << i << " x " << N << " = " << i*N << endl;
+    }
+
+    return 0;
+}
