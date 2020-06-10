21b712f0e377a9b97ff57f76538f8f6f9dbb2615
Marco <marco.schneider452@gmail.com>
2020-06-10 18:17:44 -0300

    15%error

---
+++ 1183.cpp
@@ -0,0 +1,48 @@
+//URI Online Judge - Iniciantes 1181
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    float matriz[12][12];
+    int soma, n;
+    char operation; //Caractere que definirá a operação a ser realizada
+
+    cin >> operation;
+
+    for(int i=0; i<12; i++) //Preenchimento da matriz
+    {
+        for(int j=0; j<12; j++)
+        {
+            cin >> matriz[i][j];
+        }
+    }
+
+    soma = 0;
+    n = 0;
+    for(int i=0; i<12; i++)        
+    {
+        for(int j=0; j<12; j++)
+        {
+            if(j > i) //Coluna > Linha
+            {
+                soma = soma + matriz[i][j];
+                n++;
+            }
+        }
+    }
+
+    if(operation == 'S')
+    {
+        cout << setprecision(1) << fixed << float (soma) << endl;
+    }
+    else
+    {
+        cout << setprecision(1) << fixed << (soma/float (n)) << endl;
+    }
+    
+    return 0;
+}
